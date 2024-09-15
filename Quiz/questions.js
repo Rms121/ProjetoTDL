0342ad03ac117508cba63d18305364edb49dfbe4
Dani722-code <danielle.lee@etec.sp.gov.br>
2024-09-15 14:05:58 -0300

    Add files via upload

---
+++ Quiz/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question:"O que significa TDL?",
+        answer: "D. Transtorno de Desenvolvimento da Linguagem",
+        options: [
+            "A. Taxa de Desenvolvimento Local",
+            "B. Transtorno do Déficit de Linguagem",
+            "C. To-Do-List",
+            "D. Transtorno de Desenvolvimento da Linguagem"
+        ]
+    },
+    {
+        numb: 2,
+        question:"Qual é a diferença entre TL e TDL?",
+        answer: "C. TL é o termo 'Guarda-Chuva' usado para  referir a crianças que têm dificuldades persistentes de linguagem e TDL é um Transtorno específico da Linguagem",
+        options: [
+            "A. TL afeta o desenvolvimento motor e TDL é uma facilidade persistente para adquirir e desenvolver a fala",
+            "B. TL significa Transtorno da Língua e TDL significa Transtorno do Desenvolvimento da Linguagem",
+            "C. TL é o termo 'Guarda-Chuva' usado para  referir a crianças que têm dificuldades persistentes de linguagem e TDL é um Transtorno específico da Linguagem",
+            "D. TDL significa Transtorno do Desenvolvimento da Linguagem e TL significa Técnologia de Linguagem"
+        ]
+    },
+    {
+        numb: 3,
+        question:"Assinale a opção verdadeira sobre o DEL e TDL:",
+        answer: "A. A alguns anos atrás, o DEL passou a ser nomeado de TDL",
+        options: [
+            "A. A alguns anos atrás, o DEL passou a ser nomeado de TDL",
+            "B. DELL é Distúrbio Específico de Linguagem e TDL é Transtorno do Desenvolvimento da Linguagem",
+            "C. A alguns anos atrás, o TDL passou a ser nomeado de DEL",
+            "D. DEL é um software"
+        ]
+    },
+    {
+        numb: 4,
+        question:"Qual profissional que atua para tratamento do TDL?",
+        answer: "D. Fonoaudiólogo",
+        options: [
+            "A. Médico",
+            "B. Psicólogo",
+            "C. Fisioterapeuta",
+            "D. Fonoaudiólogo"
+        ]
+    },
+    {
+        numb: 5,
+        question:"Quais são os sinais do TDL?",
+        answer: "C. A criança tem dificuldade de se expressar verbalmente",
+        options: [
+            "A. A criança pode parecer matura para a idade",
+            "B. A criança sempre lembra o que foi dito",
+            "C. A criança tem dificuldade de se expressar verbalmente",
+            "D. A criança corre muito"
+        ]
+    },
+    {
+        numb: 6,
+        question:"Escolha a alternativa INCORRETA:",
+        answer: "A. Quem tem TDAH ou outro transtorno também pode ter TDL",
+        options: [
+            "A. Quem tem TDAH ou outro transtorno também pode ter TDL",
+            "B. TDL significa Transtorno do Desenvolvimento da Linguagem",
+            "C. O profissional que trata crianças com TDL são os fonoaudiólogos",
+            "D. A Criança que sofre com TDL tem maior dificuldade de se expressar verbalmente"
+        ]
+    },
+    {
+        numb: 7,
+        question:"Quais são os sintomas e sinais comuns do TDL:",
+        answer: "B. dificuldades persistentes na aquisição e uso da linguagem",
+        options: [
+            "A. Maior sensibilidade",
+            "B. dificuldades persistentes na aquisição e uso da linguagem",
+            "C. Evita, não gosta ou se envolve contra a vontade em tarefas que exigem esforço mental prolongado",
+            "D. dificulta o aprendizado e a realização da leitura e da escrita"
+        ]
+    },
+    {
+        numb: 8,
+        question:"O que pode causar TDL?",
+        answer: "A. Pode ter relação com aspectos genéticos e embrionários",
+        options: [
+            "A. Pode ter relação com aspectos genéticos e embrionários",
+            "B. É causado por dificuldades emocionais ou exposição limitada à linguagem",
+            "C. É causado por condições médicas, como perda auditiva ou lesões celebrais",
+            "D. Quem tem autismo, geralmente, desenvolve o Transtorno do Desenvolvimento da Linguagem"
+        ]
+    },
+    {
+        numb: 9,
+        question:"Como o TDL afeta a vida de quem sofre?",
+        answer: "D. afeta crianças e persiste até a idade adulta",
+        options: [
+            "A. afeta apenas para crianças",
+            "B. afeta para crianças e os idosos",
+            "C. afeta apenas para adultos",
+            "D. afeta crianças e persiste até a idade adulta"
+        ]
+    },
+    {
+        numb: 10,
+        question:"Com qual idade pode começar a aparecer sinais que talvez indique o TDL?",
+        answer: "D. Crianças",
+        options: [
+            "A. Idosos",
+            "B. Adolescentes",
+            "C. Adultos",
+            "D. Crianças"
+        ]
+    }
+];
